5f35c677e3fb3858d06bfe1c7b0cfb118f11c56f
JesusMLares <twinlares2@hotmail.com>
2024-09-21 11:40:09 -0700

    AWS prep working. Images broken

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,30 +1,32 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react"
 
-export const CurrentUser = createContext();
+export const CurrentUser = createContext()
 
 function CurrentUserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null)
   useEffect(() => {
     const getLoggedInUser = async () => {
-      let response = await fetch(
-        "http://localhost:5000/authentication/profile",
+      // let response = await fetch(
+      //   "http://localhost:5000/authentication/profile",
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}authentication/profile`,
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         }
-      );
-      let user = await response.json();
-      setCurrentUser(user);
-    };
-    getLoggedInUser();
-  }, []);
+      )
+      let user = await response.json()
+      setCurrentUser(user)
+    }
+    getLoggedInUser()
+  }, [])
 
   return (
     <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
       {children}
     </CurrentUser.Provider>
-  );
+  )
 }
 
-export default CurrentUserProvider;
+export default CurrentUserProvider
